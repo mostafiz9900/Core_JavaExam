c5a1a81423e2de6e3af17303be29dec6718d2396
mostafiz9900 <mostafiz9900@gmail.com>
2019-05-07 04:08:27 +0600

    abstract

---
+++ ReadingConsole/src/abstractExam/A.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractExam;
+
+/**
+ *
+ * @author Mostafizur
+ */
+abstract class A {
+    abstract void callme();
+    void callmetoo(){
+        System.out.println("This is concrete method ");
+    }
+}
+
---
+++ ReadingConsole/src/abstractExam/B.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractExam;
+
+/**
+ *
+ * @author Mostafizur
+ */
+  class B extends A{
+
+    @Override
+     void callme(){
+        System.out.println("This is Abstract Method");
+     };
+    
+}
